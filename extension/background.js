9dcbbf5507a1dbf533c62a2cbe2027420d7fd0e4
garou404 <charlesmeyer347@gmail.com>
2025-03-20 09:00:32 +0100

    google sheets data harvested done

--- extension/background.js
+++ extension/background.js
@@ -4,8 +4,9 @@ const job_teaser = "www.jobteaser.com/en/job-offers";
 
 chrome.action.onClicked.addListener(async (tab) => {
     console.log("TOKEN - TOKEN - TOKEN - TOKEN - TOKEN - TOKEN - TOKEN");
-    console.log(getAuthToken());
-    console.log(tab.url);
+    let token = await getAuthToken();
+
+    fetchData(token);
     if(tab.url.includes(job_teaser)){
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
@@ -25,30 +26,54 @@ chrome.action.onClicked.addListener(async (tab) => {
 });
 
 function getAuthToken() {
-    chrome.identity.getAuthToken({interactive: true}, function(token) {
-        console.log(token);
-        return token;
+    return new Promise((resolve, reject) => {
+        chrome.identity.getAuthToken({interactive: true}, function(token) {
+            if (chrome.runtime.lastError) {
+                reject(chrome.runtime.lastError);
+            } else {
+                resolve(token);
+            }
+        });
     });
 };
-    // "content_scripts": [
-    //     {
-    //         "js": ["scripts/job_teaser.js"],
-    //         "matches": [
-    //             "https://www.jobteaser.com/*",
-    //             "https://www.jobteaser.com/en/job-offers/*"
-    //         ],
-    //         "all_frames": true
-    //     },
-    //     {
-    //         "js": ["scripts/linkedin.js"],
-    //         "matches": [
-    //             "https://www.linkedin.com/jobs/*"
-    //         ]
-    //     },
-    //     {
-    //         "js": ["scripts/indeed.js"],
-    //         "matches": [
-    //             "https://*.indeed.com/*"
-    //         ]
-    //     }
-    // ] 
+
+async function fetchData(token) {
+    console.log(token);
+    const spreadsheetId = "1OeHySMPQ_8ny9XwoGzUA7m2lrDREE0IYolxbc1FxNqk";
+    const sheetId = "crash_test";
+    const range = "!A:L";
+    const url = "https://sheets.googleapis.com/v4/spreadsheets/"
+    
+    let init = {
+        method: 'GET',
+        // async: true,
+        headers: {
+            Authorization: 'Bearer ' + token,
+            // 'Content-Type': 'application/json'
+        },
+        'contentType': 'application/x-www-form-urlencoded'
+    };
+    console.log("await fetch now, new contentType");
+    const response = await fetch(
+        url+spreadsheetId+"/values/"+sheetId+range,
+        init
+    );
+    
+    const result = await response.json();
+    console.log(result);
+    
+}
+
+// https://developers.google.com/sheets/api/reference/rest
+
+// POST /v4/spreadsheets/{spreadsheetId}:batchUpdate
+// Applies one or more updates to the spreadsheet.
+
+// GET /v4/spreadsheets/{spreadsheetId}
+// Returns the spreadsheet at the given ID.
+
+// GET /v4/spreadsheets/{spreadsheetId}/values/{range}
+// Returns a range of values from a spreadsheet.
+
+// PUT /v4/spreadsheets/{spreadsheetId}/values/{range}
+// Sets values in a range of a spreadsheet.
