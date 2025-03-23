33f219d421de15e667db5f2028a6c89071c6233d
garou404 <charlesmeyer347@gmail.com>
2025-03-23 22:48:11 +0100

    insertion of a new row to the google sheet using PUT request

--- extension/background.js
+++ extension/background.js
@@ -3,7 +3,6 @@ const linkedin = "www.linkedin.com/jobs/";
 const job_teaser = "www.jobteaser.com/en/job-offers";
 
 chrome.action.onClicked.addListener(async (tab) => {
-    console.log("TOKEN - TOKEN - TOKEN - TOKEN - TOKEN - TOKEN - TOKEN");
     let token = await getAuthToken();
 
     fetchData(token);
@@ -38,7 +37,6 @@ function getAuthToken() {
 };
 
 async function fetchData(token) {
-    console.log(token);
     const spreadsheetId = "1OeHySMPQ_8ny9XwoGzUA7m2lrDREE0IYolxbc1FxNqk";
     const sheetId = "crash_test";
     const range = "!A:L";
@@ -50,15 +48,52 @@ async function fetchData(token) {
             Authorization: 'Bearer ' + token,
         },
     };
-    console.log("await fetch now, new contentType");
     const response = await fetch(
         url+spreadsheetId+"/values/"+sheetId+range,
         init
     );
     
     const result = await response.json();
-    console.log(result);
+    addRow(result.values, "my_company", "software engineer developer director CEO", "Naves-Parmelan", token);    
+}
+
+async function addRow(data, company, position, location, token) {
+    console.log(data);
+    console.log(data.push(["test1", "test1", "test1", "test1"]));
+
+    // const headers = data[0]; // ["Name", "Age", "City"]
+    // const rows = data.slice(1); // Everything except headers
+
+    // const objects = rows.map(row => 
+    // Object.fromEntries(row.map((value, index) => [headers[index], value]))
+    // );
+
+    // console.log(objects);
+
+    const spreadsheetId = "1OeHySMPQ_8ny9XwoGzUA7m2lrDREE0IYolxbc1FxNqk";
+    const sheetId = "crash_test";
+    const range = "!A:L";
+    const url = "https://sheets.googleapis.com/v4/spreadsheets/"
     
+    const body = {
+        values: data,
+    };
+    let init = {
+        method: 'PUT',
+        headers: {
+            Authorization: 'Bearer ' + token,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body),
+    };
+
+    const response = await fetch(
+        url+spreadsheetId+"/values/"+sheetId+range+"?valueInputOption=USER_ENTERED",
+        init,
+    );
+    
+    const result = await response.json();
+    console.log(result);
 }
 
 // https://developers.google.com/sheets/api/reference/rest
